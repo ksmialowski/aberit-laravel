55689e699204c2c3ccebdbeac8a4a52d25a08ca7
Kamil Śmiałowski <smialowski.kamil@gmail.com>
2025-06-04 19:27:33 +0200

    feat: bind services in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\BrandService;
+use App\Services\CarService;
+use App\Services\OwnerService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CarService::class, function ($app) {
+            return new CarService();
+        });
+
+        $this->app->bind(OwnerService::class, function ($app) {
+            return new OwnerService();
+        });
+
+        $this->app->bind(BrandService::class, function ($app) {
+            return new BrandService();
+        });
     }
 
     /**
